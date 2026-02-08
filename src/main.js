573f03d7a40e80f693c988f1b606dccdf37998f0
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-02-08 14:37:22 +0100

    feat: integrate tailwindcss-primeui and set up animation utilities
    - Added tailwindcss-primeui as a dependency.
    - Created new CSS files for animations, colors, and utility classes.
    - Implemented keyframes for various animations including fade, slide, and zoom effects.
    - Updated main application styles to include tailwindcss and primeicons.
    - Refactored App.vue to use RouterView and added a sidebar component.
    - Created template components for authentication and loading states.
    - Updated main.js to configure PrimeVue with the Aura theme.
    - Added new pages for Dashboard and Landing.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.mount('#app')
