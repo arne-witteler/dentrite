872ce4fbeb4be8aa714607c99a291ee5625f6ee3
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-02-08 13:40:37 +0100

    restart project

--- src/pages/ModulesPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div>
-    <h1>Module</h1>
-    <p>Hier kommt später die Liste deiner Kurse hin.</p>
-  </div>
-</template>
--- src/pages/ProfilePage.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Profil</h1>
-    <p>Hier kommt später dein Profil hin.</p>
-  </div>
-</template>
